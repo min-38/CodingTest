40b7856774a0d4bf9ee32f4270fcd3cfc6724e5d
min-38 <mindino03@gmail.com>
2024-07-05 02:46:07 +0900

    프로그래머스 - 튜플 solved
    https://school.programmers.co.kr/learn/courses/30/lessons/64065

---
+++ 프로그래머스/day31/2019 카카오 개발자 겨울 인턴십/튜플/64065.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+void Insert(vector<int>& answer, int value) {
+    bool exist = false;
+    for(int i = 0; i < answer.size(); i++)
+        if(answer[i] == value) {
+            exist = true;
+            break;
+        }
+    
+    if(!exist)
+        answer.push_back(value);
+}
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    vector<vector<int>> v;
+    
+    int cursor = 1;
+    while(cursor < s.length() - 1) {
+        vector<int> elmnts;
+
+        if(s[cursor] == '{') {
+            string tmp = "";
+            while(s[cursor] != '}') {
+                cursor++; // 커서 1 증가
+                if(s[cursor] == ',' || s[cursor] == '}') {
+                    elmnts.push_back(stoi(tmp));
+                    tmp = "";
+                    continue;
+                }
+                tmp += s[cursor];
+            }
+            v.push_back(elmnts);
+        }
+        cursor++;
+    }
+
+    int cnt = 1;
+    while(cnt <= v.size()) {
+        for(int i = 0; i < v.size(); i++)
+            if(v[i].size() == cnt)
+                for(int j = 0; j < cnt; j++)
+                    Insert(answer, v[i][j]);
+        cnt++;
+    }
+    return answer;
+}
+
+int main() {
+    solution("{{4,2,3},{3},{2,3,4,1},{2,3}}");
+    return 0;
+}
