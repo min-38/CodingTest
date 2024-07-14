20be5bee76bb6d7ef1c13aaf1492ad9e3b8e546b
min-38 <mindino03@gmail.com>
2024-07-14 21:04:49 +0900

    프로그래머스 - 점프와 순간 이동 solved
    https://school.programmers.co.kr/learn/courses/30/lessons/12980

---
+++ 프로그래머스/day34/Summer&Winter Coding(~2018)/12980.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 1;
+
+    while(n > 1) {
+        if(n % 2 > 0)
+            ans++;
+        n /= 2;
+    }
+
+    return ans;
+}
