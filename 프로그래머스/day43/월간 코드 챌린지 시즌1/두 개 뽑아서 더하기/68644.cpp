45747b5f5bb30b9c349a6f23a6e73ff6e3d0a8f6
min-38 <mindino03@gmail.com>
2024-08-23 02:08:22 +0900

    프로그래머스 - 두 개 뽑아서 더하기 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/68644

--- 프로그래머스/day43/월간 코드 챌린지 시즌1/두 개 뽑아서 더하기/68644.cpp
+++ 프로그래머스/day43/월간 코드 챌린지 시즌1/두 개 뽑아서 더하기/68644.cpp
@@ -1,33 +1,14 @@
-#include <string>
+#include <set>
 #include <vector>
 
 using namespace std;
 
 vector<int> solution(vector<int> numbers) {
-    vector<int> answer;
-    
-    vector<int> v;
+    set<int> s;
     for(int i = 0; i < numbers.size(); i++)
         for(int j = i + 1; j < numbers.size(); j++)
-            v.push_back(numbers[i] + numbers[j]);
-
-    // 선택 정렬
-    for(int i = 0; i < v.size(); i++) {
-        int l_idx = i; // least index;
-        for(int j = i + 1; j < v.size(); j++)
-            if(v[l_idx] > v[j])
-                l_idx = j;
-        
-        if(v[l_idx] != v[i]) {
-            int tmp = v[l_idx];
-            v[l_idx] = v[i];
-            v[i] = tmp;
-        }
-    }
-
-    answer.push_back(v[0]);
-    for(int i = 1; i < v.size(); i++)
-        if(answer.back() != v[i])
-            answer.push_back(v[i]);
+            s.insert(numbers[i] + numbers[j]);
+    
+    vector<int> answer(s.begin(), s.end());
     return answer;
 }
