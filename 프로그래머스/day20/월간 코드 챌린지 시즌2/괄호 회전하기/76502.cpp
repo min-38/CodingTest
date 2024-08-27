d503756a22aae2b3366a136d43dbcd3172e515e1
min-38 <mindino03@gmail.com>
2024-08-28 00:03:47 +0900

    프로그래머스 - 괄호 회전하기 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/76502

--- 프로그래머스/day20/월간 코드 챌린지 시즌2/괄호 회전하기/76502.cpp
+++ 프로그래머스/day20/월간 코드 챌린지 시즌2/괄호 회전하기/76502.cpp
@@ -1,44 +1,43 @@
-#include <string>
-#include <vector>
 #include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-int solution(string s) {
-    int answer = -1;
-    
-    int x = 0;
-    int len = s.length(); // 문자열의 길이
-    string tmp = s;
-    stack<char> st;
-    for(int i = 0; i < len; i++) {
-        while(!st.empty()) st.pop(); // 시작 전 stack 비우기
+// 닫힌 괄호의 짝을 바로 확인할 수 있도록 맵 선언
+unordered_map<char, char> bracketPair = {{')', '('}, {']', '['}, {'}', '{'}};
 
-        for(char c : tmp) {
-            char top;
-            if(st.size() > 0) {
-                top = st.top();
-                if((top == '(' && c == ')')
-                    || (top == '{' && c == '}')
-                    || (top == '[' && c == ']')) {
-                    st.pop();
-                    continue;
-                }
-            }
-            st.push(c);
+// 인수로 받은 문자열 기준 괄호의 짝이 맞는지 확인
+bool isValid(string &s, int start) {
+    stack<char> stk;
+    unsigned int sz = s.size();
+
+    // 문자열을 순회하면서
+    for(int i = 0; i < sz; i++) {
+        char ch = s[(start + i) % sz];
+        // ch가 닫힌 괄호인 경우
+        if(bracketPair.count(ch)) {
+            // 스택이 비었거나 top 원소가 ch와 짝이 맞는 열린 괄호가 아니면 false 반환
+            if(stk.empty() || stk.top() != bracketPair[ch])
+                return false;
+            // ch와 짝이 맞는 열린 괄호면 해당 열린 괄호 제거
+            stk.pop();
+        } else {
+            stk.push(ch); // 열린 괄호면 스택에 푸시
         }
-        if(st.empty()) {
-            x++;
-        }
-        
-        // 문자열 왼쪽으로 밀기
-        char move = tmp[0];
-        for(int j = 1; j < len; j++) {
-            tmp[j - 1] = tmp[j];
-        }
-        tmp[len - 1] = move;
     }
-    
-    answer = x;
+    // 스택이 비었으면 true를 반환(괄호 짝이 맞다는 의미)
+    return stk.empty();
+}
+
+int solution(string s) {
+    int answer = 0;
+    int n = s.size();
+
+    // 문자열을 순회하면서 괄호의 짝이 맞으면 answer를 1 증가 시킴
+    for(int i = 0; i < n; i++)
+        answer += isValid(s, i);
+
     return answer;
 }
