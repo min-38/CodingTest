5ad93f53b9fd561b83986001fbab07acb26bf559
min-38 <mindino03@gmail.com>
2024-08-28 00:03:21 +0900

    프로그래머스 - 행렬의 곱셈 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/12949

--- 프로그래머스/day35/연습문제/행렬의 곱셈/12949.cpp
+++ 프로그래머스/day35/연습문제/행렬의 곱셈/12949.cpp
@@ -3,29 +3,15 @@
 
 using namespace std;
 
-int calculate(vector<int> row, vector<int> column) {
-    int result = 0;
-    for(int i = 0; i < row.size(); i++)
-        result += row[i] * column[i];
-    return result;
-}
-
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
     vector<vector<int>> answer;
-        
-    for(int i = 0; i < arr1.size(); i++) {
-        vector<int> sub_answer;
 
-        int column_idx = 0;
-        while(column_idx < arr2[0].size()) {
-            vector<int> column;
-            for(int j = 0; j < arr2.size(); j++)
-                column.push_back(arr2[j][column_idx]);
+    answer.assign(arr1.size(), vector<int>(arr2[1].size(), 0));
 
-            sub_answer.push_back(calculate(arr1[i], column));
-            column_idx++;
-        }
-        answer.push_back(sub_answer);
-    }
+    for(int i = 0; i < arr1.size(); i++)
+        for(int j = 0; j < arr2[1].size(); j++)
+            for(int k = 0; k < arr2.size(); k++)
+                answer[i][j] += arr1[i][k] * arr2[k][j];
+
     return answer;
 }
