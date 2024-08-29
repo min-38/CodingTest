224c3df04adb9c50dc9c5a9d842bce2a87826935
min-38 <mindino03@gmail.com>
2024-08-29 14:15:59 +0900

    프로그래머스 - 전화번호 목록 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/42577

--- 프로그래머스/day46/해시/전화번호 목록/42577.cpp
+++ 프로그래머스/day46/해시/전화번호 목록/42577.cpp
@@ -1,14 +1,33 @@
 #include <string>
+#include <unordered_map>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-bool solution(vector < string > phone_book) {
-    sort(phone_book.begin(), phone_book.end());
+// 전화번호부를 저장하기 위한 해시맵
+unordered_map<string, int> phoneBook;
 
-    for(int i = 0; i < phone_book.size() - 1; i++)
-        if(phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].length()))
+// 번호가 접두어인지 확인하는 함수
+bool isPrefix(const string& phoneNumber) {
+    string prefix = "";
+    for(char digit : phoneNumber) {
+        prefix += digit;
+        // 현재 접두어가 전화번호부에 있고 현재 번호와 같이 않다면 = 접두어
+        if(phoneBook.find(prefix) != phoneBook.end() && prefix != phoneNumber)
+            return true;
+    }
+    // 접두어가 아니면 false 반환
+    return false;
+}
+
+bool solution(vector<string> phoneNumbers) {
+    // 전화번호부를 해시맵에 저장
+    for(const string& phoneNumber : phoneNumbers)
+        phoneBook[phoneNumber] = 1;
+
+    // 각 전화번호가 접두어인지 확인
+    for(const string& phoneNumber : phoneNumbers)
+        if(isPrefix(phoneNumber))
             return false;
 
     return true;
