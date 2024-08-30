708222f6a6abc0f7d6be7cd9e3ad4f83bcb97c59
min-38 <mindino03@gmail.com>
2024-08-30 22:25:26 +0900

    프로그래머스 - 신고 결과 받기 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/92334

--- 프로그래머스/day22/2022 KAKAO BLIND RECRUITMENT/신고 결과 받기/92334.cpp
+++ 프로그래머스/day22/2022 KAKAO BLIND RECRUITMENT/신고 결과 받기/92334.cpp
@@ -1,62 +1,34 @@
+#include <sstream>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
-#include <map>
 
 using namespace std;
 
-void split(string arr[], string s) {
-    int index = 0; 
-
-    for(int i = 0; i < s.length(); i++) {
-        char c = s[i];
-        if(c == ' ') {
-            index++;
-            continue;
-        }
-        arr[index] += c;
-    }
-}
-
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    // 신고당한 유저 - 신고 유저 셋
+    unordered_map<string, unordered_set<string>> reported_user;
+    unordered_map<string, int> count; // 처리 결과 메일을 받은 유저 - 받은 횟수
+    
+    // 신고 기록 순회
+    for(string &r : report) {
+        // 각 report에서 user_id와 report_id를 분리하고 reported_user에 추가
+        stringstream ss(r);
+        string user_id, reported_id;
+        ss >> user_id >> reported_id;
+        reported_user[reported_id].insert(user_id);
+    }
+
+    for(auto &[reported_id, user_id_lst] : reported_user)
+        if(user_id_lst.size() >= k) // k명 이상에게 신고당했는지 확인
+            for(const string &uid : user_id_lst) // 각 uid가 신고당한 횟수 기록
+                count[uid]++;
+
     vector<int> answer;
+    // 아이디별 메일 받은 횟수를 id_list 순서대로 answer에 추가
+    for(string &id : id_list)
+        answer.push_back(count[id]);
     
-    map<string, vector<string>> um;
-    map<string, int> um2;
-
-    for(string id : id_list) {
-        vector<string> v;
-        um.insert(make_pair(id, v));
-
-        um2.insert(make_pair(id, 0));
-    }
-
-    for(string s : report) {
-        string arr[2] = {"", };
-        
-        split(arr, s);
-
-        bool isAlreadyReported = false;
-        vector<string> *v = &um[arr[0]];
-        for(string s2 : *v)
-            if(s2 == arr[1])
-                isAlreadyReported = true;
-        
-        if(!isAlreadyReported) {
-            v->push_back(arr[1]);
-            um2[arr[1]]++;
-        }
-    }
-    
-    for(string s : id_list) {
-        vector<string> *v = &um[s];
-        int banned = 0;
-        
-        for(string s2 : *v) {
-            if(um2[s2] >= k)
-                banned++;
-        }
-        answer.push_back(banned);
-    }
-
     return answer;
 }
