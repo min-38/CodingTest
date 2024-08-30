a3349f97315a31d1b2a8920fced079697e6ecfab
min-38 <mindino03@gmail.com>
2024-08-30 18:18:16 +0900

    프로그래머스 - 베스트앨범 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/42579

--- 프로그래머스/day47/해시/베스트앨범/42579.cpp
+++ 프로그래머스/day47/해시/베스트앨범/42579.cpp
@@ -1,34 +1,43 @@
-#include <string>
-#include <vector>
-#include <unordered_map>
 #include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
+bool compareGenre(const pair<string, int> &a, const pair<string, int> &b) {
+    return a.second > b.second;
+}
+
+bool compareSong(const pair<int, int> &a, const pair<int, int> &b) {
+    if(a.second == b.second)
+        return a.first < b.first;
+    return a.second > b.second;
+}
+
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
-    
-    unordered_map<string, int> um;
-    for(int i = 0; i < genres.size(); i++)
-        um[genres[i]] += plays[i];
-    
-    vector<pair<string, int>> vec(um.begin(), um.end());
-    sort(vec.begin(), vec.end(), [](const auto &a, const auto &b) {
-            return a.second > b.second;
-    });
+    unordered_map<string, vector<pair<int, int>>> genres_dict;
+    unordered_map<string, int> play_dict;
 
-    for (const auto &pair : vec) {
-        int limit = 0;
-        while(limit++ < 2) {
-            int max_idx = -1;
-            for(int i = 0; i < plays.size(); i++)
-                if(pair.first == genres[i] && (max_idx < 0 && plays[i] != -1 || plays[max_idx] < plays[i]))
-                    max_idx = i;
-            if(max_idx == -1)
-                break;
-            answer.push_back(max_idx);
-            plays[max_idx] = -1;
-        }
+    // 장르별 총 재생 횟수와 각 곡의 재생 횟수 저장
+    for(int i = 0; i < genres.size(); i++) {
+        genres_dict[genres[i]].push_back({i, plays[i]});
+        play_dict[genres[i]] += plays[i];
     }
+
+    // 총 재생 횟수가 많은 장르순으로 정렬
+    vector<pair<string, int>> sorted_genres(play_dict.begin(), play_dict.end());
+    sort(sorted_genres.begin(), sorted_genres.end(), compareGenre);
+
+    // 각 장르 내에서 노래를 재생 횟수순으로 정렬해 최대 2곡까지 선택
+    for(auto &genre : sorted_genres) {
+        auto &songs = genres_dict[genre.first];
+        sort(songs.begin(), songs.end(), compareSong);
+
+        for(int i = 0; i < min(2, (int)songs.size()); ++i)
+            answer.push_back(songs[i].first);
+    }
+
     return answer;
 }
