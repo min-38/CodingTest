c02b12ffa88c16a87d08edbf477e73f62aaa08db
min-38 <mindino03@gmail.com>
2024-09-03 03:10:32 +0900

    프로그래머스 - 다단계 칫솔 판매 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/77486

--- 프로그래머스/day48/2021 Dev-Matching: 웹 백엔드 개발자(상반기)/다단계 칫솔 판매/77486.cpp
+++ 프로그래머스/day48/2021 Dev-Matching: 웹 백엔드 개발자(상반기)/다단계 칫솔 판매/77486.cpp
@@ -1,32 +1,44 @@
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
-    vector<int> answer(enroll.size(), 0);
+    unordered_map<string, string> parent;
 
-    vector<int> v(enroll.size(), 0);
-    for(int i = 0; i < enroll.size(); i++)
-        for(int j = 0; j < enroll.size(); j++) {
-            if(referral[i] == "-")
-                v[i] = -1;
-            else if(referral[i] == enroll[j])
-                v[i] = j;
-        }
+    // parent는 판매원 - 판매원을 참여시킨 추천인으로 구성
+    for(size_t i = 0; i < enroll.size(); ++i)
+        parent[enroll[i]] = referral[i];
+    
+    unordered_map<string, int> total;
+    // total은 판매원 - 판매원의 수익으로 구성, 수익을 0으로 초기화
+    for(const auto &name: enroll)
+        total[name] = 0;
+    
+    for(size_t i = 0; i < enroll.size(); ++i) {
+        int money = amount[i] * 100; // 현재 판매원의 수익금
+        string cur_name = seller[i]; // 실제 물건을 판 사람
 
-    for(int i = 0; i < seller.size(); i++) {
-        int index;
-        for(index = 0; index < enroll.size(); index++)
-            if(enroll[index] == seller[i])
+        while(money > 0 && cur_name != "-") {
+            // 실제 물건을 판 사람을 기준으로 추천인을 계속 추적하며 남은 수익의 10%를 분배
+            int to_distribute = money / 10;
+            total[cur_name] += money - to_distribute;
+            // 현재 이름의 추천인이 있으면 현재 이름을 추천인으로 변경, 그렇지 않으면 종료
+            if(parent.find(cur_name) != parent.end())
+                cur_name = parent[cur_name];
+            else
                 break;
-        amount[i] *= 100;
-
-        while(index != -1 && amount[i] > 0) {
-            answer[index] += amount[i] - (amount[i] * 10 / 100);
-            amount[i] = amount[i] * 10 / 100;
-            index = v[index];
+            // 현재 판매원이 추천인으로 변경되었으므로 수익금도 이에 맞춰 업데이트
+            money = to_distribute;
         }
     }
-    return answer;
+
+    // 수익금을 answer에 업데이트해서 반환
+    vector<int> result;
+    result.reserve(enroll.size());
+    for(const auto &name : enroll)
+        result.push_back(total[name]);
+
+    return result;
 }
