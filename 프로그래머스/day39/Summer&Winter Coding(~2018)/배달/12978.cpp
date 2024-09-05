1e1e4922fe92efb14922d6ad6894028c092cd5a0
min-38 <mindino03@gmail.com>
2024-09-06 03:24:06 +0900

    프로그래머스 - 배달 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/12978

--- 프로그래머스/day39/Summer&Winter Coding(~2018)/배달/12978.cpp
+++ 프로그래머스/day39/Summer&Winter Coding(~2018)/배달/12978.cpp
@@ -1,48 +1,55 @@
-#include <iostream>
+#include <limits>
+#include <queue>
 #include <vector>
+
 using namespace std;
 
-struct Node {
-    vector<int> time;
-    vector<int> connected;
-    int min_length;
-};
-
-void deliver(vector<Node>& v, const int K, const int location, int duration, vector<bool>& visited) {
-    Node* node = &v[location];
-    if(node->min_length == 0 || node->min_length > duration)
-        node->min_length = duration;
-
-    for(int i = 0; i < node->connected.size(); i++)
-        if(!visited[node->connected[i]])
-            if(duration + node->time[i] <= K) {
-                visited[node->connected[i]] = true;
-                deliver(v, K, node->connected[i], duration + node->time[i], visited);
-                visited[node->connected[i]] = false;
-            }
-}
-
 int solution(int N, vector<vector<int> > road, int K) {
-    int answer = 0;
-    
-    vector<Node> v(N + 1);
+    vector<pair<int, int>> graph[N + 1];
+    vector<int> distances(N + 1, numeric_limits<int>::max());
     vector<bool> visited(N + 1, false);
-    for(int i = 0; i < road.size(); i++) {
-        v[road[i][0]].connected.push_back(road[i][1]);
-        v[road[i][0]].time.push_back(road[i][2]);
-        v[road[i][0]].min_length = K;
+    distances[1] = 0;
+    
+    // 방향이 따로 없으므로 양방향 동일한 가중치
+    for(const auto &r : road) {
+        int a = r[0], b = r[1], cost = r[2];
+        graph[a].push_back({b, cost});
+        graph[b].push_back({a, cost});
+    }
 
-        v[road[i][1]].connected.push_back(road[i][0]);
-        v[road[i][1]].time.push_back(road[i][2]);
-        v[road[i][1]].min_length = K;
+    // 출발점을 heap에 추가
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+    heap.push({0, 1});
+
+    while(!heap.empty()) {
+        int dist = heap.top().first;
+        int node = heap.top().second;
+        heap.pop();
+
+        // 이미 방문한 노드는 무시
+        if(visited[node])
+            continue;
+        visited[node] = true;
+
+        for(const auto &next : graph[node]) {
+            int next_node = next.first;
+            int next_dist = next.second;
+            int cost = dist + next_dist;
+
+            // 거쳐가는 노드로 가는 경로의 비용이 더 짧으면 최소 비용 갱신
+            if(cost < distances[next_node]) {
+                distances[next_node] = cost;
+                heap.push({cost, next_node});
+            }
+        }
     }
     
-    visited[1] = true;
-    deliver(v, K, 1, 0, visited);
-    
-    for(int i = 1; i < v.size(); i++)
-        if(v[i].min_length <= K)
-            answer++;
+    int count = 0;
 
-    return answer;
+    // 거리가 K 이하인 장소를 카운트
+    for(int i = 1; i<= N; i++)
+        if(distances[i] <= K)
+            count++;
+    
+    return count;
 }
