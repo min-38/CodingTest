d8ea0986e8681087c90d558532a1a6e65dfcb50b
min-38 <mindino03@gmail.com>
2024-09-07 19:50:41 +0900

    프로그래머스 - 피로도 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/87946

--- 프로그래머스/day37/완전탐색/피로도/87946.cpp
+++ 프로그래머스/day37/완전탐색/피로도/87946.cpp
@@ -3,42 +3,31 @@
 
 using namespace std;
 
-struct Node {
-    int k; // 남은 피로도
-    vector<bool> done; // 완료한 던전
-    int done_cnt = 0; // 완료한 던전 개수
+int maxDepth = 0;
+bool visited[8] {
+    false,
 };
 
-void run(int max, const vector<vector<int>>& dungeons, Node node, int& answer) {
-    if(node.done_cnt < dungeons.size())
-        for(int i = 0; i < dungeons.size(); i++) {
-            if(node.done[i])// 이미 완료한 던전
-                continue;
-
-            if(dungeons[i][0] <= node.k)
-                if(node.k - dungeons[i][1] > 0) {
-                    Node newNode;
-                    newNode.k = node.k - dungeons[i][1];
-                    newNode.done = node.done;
-                    newNode.done[i] = true;
-                    newNode.done_cnt = node.done_cnt + 1;
-
-                    run(max, dungeons, newNode, answer);
-                }
-        }
-
-    if(node.done_cnt > answer)
-        answer = node.done_cnt;
+// 던전의 최대 방문수를 갱신하면서 깊이 우선 탐색으로 던전 탐험
+void exploreDungeon(int depth, int power, vector<vector<int>> &dungeons) {
+    if(maxDepth < depth)
+        maxDepth = depth;
+    
+    for(int i = 0; i < dungeons.size(); i++) {
+        // 이미 방문한 던전이거나, 최소 필요 피로도가 현재 남은 피로도보다 많을 때
+        if(visited[i] || dungeons[i][0] > power)
+            continue;
+        
+        // 방문이 가능한 모든 경우 확인
+        visited[i] = true;
+        exploreDungeon(depth + 1, power - dungeons[i][1], dungeons);
+        visited[i] = false;
+    }
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
-    int answer = -1;
-    
-    Node node;
-    node.k = k;
-    node.done = vector<bool>(dungeons.size(), false);
-    
-    run(k, dungeons, node, answer);
+    // 던전 탐색 시작
+    exploreDungeon(0, k, dungeons);
 
-    return answer;
+    return maxDepth;
 }
