e599432db34345baf492ef9a285b1acc73ab23a3
min-38 <mindino03@gmail.com>
2024-09-12 14:43:26 +0900

    프로그래머스 - 정수 내림차순으로 배치하기 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/12933

--- 프로그래머스/day52/연습문제/정수 내림차순으로 배치하기/12933.cpp
+++ 프로그래머스/day52/연습문제/정수 내림차순으로 배치하기/12933.cpp
@@ -1,22 +1,13 @@
-#include <string>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
 long long solution(long long n) {
-    long long answer = 0;
-    
-    string sn = to_string(n);
-    
-    for(int key = 1; key < sn.length(); key++)
-        for(int i = 0; i < key; i++)
-            if(sn[i] - '0' < sn[key] - '0') {
-                char c = sn[key];
-                for(int j = key; j > i; j--)
-                    sn[j] = sn[j - 1];
-                sn[i] = c;
-                break;
-            }
-    
-    return stoll(sn);
+    // 숫자를 문자열로 변환
+    string str = to_string(n);
+
+    // 역순으로 정렬
+    sort(str.rbegin(), str.rend());
+    return stoll(str);
 }
