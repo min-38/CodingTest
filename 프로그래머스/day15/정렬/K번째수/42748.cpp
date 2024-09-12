59197fe99fb649176a3eb3ae24cd6b7dc09f121e
min-38 <mindino03@gmail.com>
2024-09-12 14:58:59 +0900

    프로그래머스 - K번째수 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/42748

--- 프로그래머스/day15/정렬/K번째수/42748.cpp
+++ 프로그래머스/day15/정렬/K번째수/42748.cpp
@@ -1,24 +1,21 @@
+#include <algorithm>
 #include <string>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
-    
-    vector<int> v;
-    int i, j, k;
-    for(int index = 0; index < commands.size(); index++) {
-        i = commands[index][0];
-        j = commands[index][1];
-        k = commands[index][2];
-        
-        v.clear();
-        for(int index2 = i - 1; index2 < j; index2++) {
-            v.push_back(array[index2]);
-        }
-        sort(v.begin(), v.end());
-        answer.push_back(v[k - 1]);
+    vector<int> subArray;
+
+    for(const auto &cmd : commands) {
+        // array에서 각 cmd에 해당하는 영역만 subArray로 복사 후 정렬
+        subArray.assign(array.begin() + (cmd[0] - 1), array.begin() + cmd[1]);
+        sort(subArray.begin(), subArray.end());
+
+        // subArray에서 k번째 answer에 추가
+        answer.push_back(subArray[cmd[2] - 1]);
     }
+
     return answer;
 }
