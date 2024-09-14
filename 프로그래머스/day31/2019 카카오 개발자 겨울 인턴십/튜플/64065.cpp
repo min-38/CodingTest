95933cf4aa57a1df7fa4d39fc0d577e0ddef361d
min-38 <mindino03@gmail.com>
2024-09-14 21:10:33 +0900

    프로그래머스 - 튜플 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/64065

--- 프로그래머스/day31/2019 카카오 개발자 겨울 인턴십/튜플/64065.cpp
+++ 프로그래머스/day31/2019 카카오 개발자 겨울 인턴십/튜플/64065.cpp
@@ -1,51 +1,46 @@
+#include <algorithm>
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
-void Insert(vector<int>& answer, int value) {
-    bool exist = false;
-    for(int i = 0; i < answer.size(); i++)
-        if(answer[i] == value) {
-            exist = true;
-            break;
+int counts[100001] = {};
+
+void updateCounts(const string &s) {
+    string numStr;
+    // 인수로 받은 문자열 순회
+    for(char ch : s) {
+        // 현재 문자가 숫자일 때
+        if(isdigit(ch)) {
+            numStr += ch;
+            // 현재 문자가 숫자가 아닐 때
+        } else {
+            if(!numStr.empty()) {
+                // 계수 정렬을 하기 위해 각 숫자의 개수 저장
+                counts[stoi(numStr)]++;
+                numStr.clear();
+            }
         }
-    
-    if(!exist)
-        answer.push_back(value);
+    }
 }
 
 vector<int> solution(string s) {
     vector<int> answer;
-    vector<vector<int>> v;
-    
-    int cursor = 1;
-    while(cursor < s.length() - 1) {
-        vector<int> elmnts;
+    // 집합이 담긴 문자열의 각 원소를 계수 정렬
+    updateCounts(s);
 
-        if(s[cursor] == '{') {
-            string tmp = "";
-            while(s[cursor] != '}') {
-                cursor++; // 커서 1 증가
-                if(s[cursor] == ',' || s[cursor] == '}') {
-                    elmnts.push_back(stoi(tmp));
-                    tmp = "";
-                    continue;
-                }
-                tmp += s[cursor];
-            }
-            v.push_back(elmnts);
-        }
-        cursor++;
-    }
+    vector<pair<int, int>> freqPairs;
+    for(int i = 1; i < 100000; i++)
+        // 집합에 있는 원소면 (개수, 값) 형식으로 푸시
+        if(counts[i] > 0)
+            freqPairs.push_back({counts[i], i});
 
-    int cnt = 1;
-    while(cnt <= v.size()) {
-        for(int i = 0; i < v.size(); i++)
-            if(v[i].size() == cnt)
-                for(int j = 0; j < cnt; j++)
-                    Insert(answer, v[i][j]);
-        cnt++;
-    }
+    // 각 원소의 개수를 기준으로 내림차순 정렬
+    sort(freqPairs.rbegin(), freqPairs.rend());
+
+    // 원소의 개수로 내림차순 정렬된 벡터를 순회하며 원소를 푸시
+    for(const auto &p : freqPairs)
+        answer.push_back(p.second);
+
     return answer;
 }
