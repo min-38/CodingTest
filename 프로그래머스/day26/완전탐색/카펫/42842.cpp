8f06a2a005f7798104a755257a7510702adffbaf
min-38 <mindino03@gmail.com>
2024-09-14 22:00:28 +0900

    프로그래머스 - 카펫 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/42842

--- 프로그래머스/day26/완전탐색/카펫/42842.cpp
+++ 프로그래머스/day26/완전탐색/카펫/42842.cpp
@@ -1,27 +1,21 @@
-#include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
-    vector<int> answer;
-    
-    int tiles = brown + yellow;
-    for(int i = tiles; i > 0; i--)
-        if(tiles % i == 0)
-        {
-            int x = i;
-            int y = tiles / i;
-            
-            if(x < y)
-                break;
-            
-            if((x - 2) * (y - 2) == yellow)
-            {
-                answer.push_back(x);
-                answer.push_back(y);
+    // 격자의 총 개수 (갈색 격자 + 노란색 격자)
+    int total_size = brown + yellow;
+    // 세로 길이의 범위는 3부터 (갈색 격자 + 노란색 격자)의 제곱근
+    for(int vertical = 3; vertical <= sqrt(total_size); ++vertical) {
+        // 사각형 구성이 되는지 확인
+        if(total_size % vertical == 0) {
+            int horizontal = total_size / vertical; // 사각형의 가로 길이
+            // 카펫 형태로 만들 수 있는지 확인
+            if(brown == (horizontal + vertical - 2) * 2) {
+                return {horizontal, vertical}; // {가로 길이, 세로 길이}
             }
         }
-    
-    return answer;
+    }
+    return {}; // 만약 답을 찾지 못했다면 빈 벡터를 반환
 }
