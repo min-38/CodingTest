fe71214eb0da4f5e954696782b6209c84f95aa8b
min-38 <mindino03@gmail.com>
2024-12-16 01:19:12 +0900

    프로그래머스 - 숫자 짝꿍 fixed
    https://school.programmers.co.kr/learn/courses/30/lessons/131128

--- 프로그래머스/day61/연습문제/숫자 짝꿍/131128.cpp
+++ 프로그래머스/day61/연습문제/숫자 짝꿍/131128.cpp
@@ -1,41 +1,27 @@
 #include <string>
 #include <vector>
-#include <map>
 #include <algorithm>
 
 using namespace std;
 
 string solution(string X, string Y) {
-    string answer = "";
-
-    vector<int> v;
-    map<int, int> mx, my;
-
-    for (char c : X)
-        mx[c - '0']++;
-    for (char c : Y)
-        my[c - '0']++;
+    vector<int> countX(10, 0), countY(10, 0), common;
+    
+    for (char c : X) countX[c - '0']++;
+    for (char c : Y) countY[c - '0']++;
 
     for (int i = 0; i < 10; i++)
-        if(mx[i] > 0 && my[i] > 0) {
-            int cnt = mx[i];
-            if(mx[i] > my[i])
-                cnt = my[i];
+        for (int j = 0; j < min(countX[i], countY[i]); j++)
+            common.push_back(i);
 
-            for (int j = 0; j < cnt; j++)
-                v.push_back(i);
-        }
-    
-    if(v.size() == 0)
-        return "-1";
+    if (common.empty()) return "-1";
+    if (common[0] == 0 && common.size() == 1) return "0";
 
-    sort(v.rbegin(), v.rend());
-    
-    if(v[0] == 0)
-        return "0";
-    
-    for (int i : v)
-        answer += i + '0';
+    sort(common.rbegin(), common.rend());
+    if (common[0] == 0) return "0";
+
+    string answer;
+    for (int num : common) answer += to_string(num);
 
     return answer;
 }
