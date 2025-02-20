4fee32436d1f5c4af9ab6a46793d9d8d904343ab
min-38 <mindino03@gmail.com>
2025-02-20 14:58:22 +0900

    백준 - 수 묶기 fixed
    https://www.acmicpc.net/problem/1744

--- 백준/푼 문제/1744.cpp
+++ 백준/푼 문제/1744.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
 int main() {
@@ -7,34 +8,67 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N; // 카드 묶음 수
+    int N;  // 수열의 크기
     cin >> N;
-    priority_queue<int, vector<int>, greater<int> > pq; // 오름차순 정렬
-    int data;
+    
+    priority_queue<int, vector<int>> positive_pq;                 // 양수 우선수위 큐
+    priority_queue<int, vector<int>, greater<int>> negative_pq;   // 음수 우선수위 큐
+    int one_cnt = 0, zero_cnt = 0;                                // 1 개수, 0 개수
 
-    for (int i = 0; i < N; i++) {
-        cin >> data;
-        pq.push(data);
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        cin >> num;
+
+        if (num == 0)
+            zero_cnt++;
+        else if (num == 1)
+            one_cnt++;
+        else if (num > 1)
+            positive_pq.push(num);
+        else if (num < 0)
+            negative_pq.push(num);
     }
 
-    int data1 = 0;
-    int data2 = 0;
     int result = 0;
 
-    while(pq.size() != 1) {
-        data1 = pq.top();
-        pq.pop();
-        data2 = pq.top();
-        pq.pop();
-        if((data1 < 0 && data2 < 0) || (data1 > 1 && data2 > 1)) {
-            result += data1 * data2;
-        } else {
-            result += data1 + data2;
-        }
-        // pq.push(data1 + data2);
+    while (negative_pq.size() > 1)
+    {
+        int a = negative_pq.top(); negative_pq.pop();
+        int b = negative_pq.top(); negative_pq.pop();
+
+        result += a * b;
     }
 
-    cout << result << endl;
+    if (!negative_pq.empty())
+        if (zero_cnt == 0)
+        {
+            result += negative_pq.top();
+            negative_pq.pop();
+        }
+        else
+        {
+            --zero_cnt;
+        }
+
+
+    while (positive_pq.size() > 1)
+    {
+        int a = positive_pq.top(); positive_pq.pop();
+        int b = positive_pq.top(); positive_pq.pop();
+
+        result += a * b;
+    }
+
+    if (!positive_pq.empty())
+    {
+        result += positive_pq.top();
+        positive_pq.pop();
+    }
+    
+    result += one_cnt;
+
+    cout << result << "\n";
 
     return 0;
 }
